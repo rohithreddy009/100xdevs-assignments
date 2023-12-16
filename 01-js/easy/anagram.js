454bd2f6de5f165837719f59209820fd8e82478f
Rohith Reddy <yrohithreddy12@gmail.com>
2023-12-17 00:16:20 +0530

    answers submission

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
+  // Remove spaces and convert to lowercase for case-insensitive comparison
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
 
+  // Check if the sorted characters of both strings are the same
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
+
+
