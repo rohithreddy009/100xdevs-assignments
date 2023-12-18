0d5bbc53c56be392be62e02cb47a361da2e8fb68
rohithreddy <yrohithreddy12@gmail.com>
2023-12-18 16:25:54 +0530

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -1,25 +1,10 @@
 function isPalindrome(str) {
-  // Remove spaces and punctuation marks and convert the string to lowercase
-  const cleanedStr = str.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase();
-
-  // Initialize pointers for the start and end of the cleaned string
-  let start = 0;
-  let end = cleanedStr.length - 1;
-
-  // Loop until the start pointer is less than the end pointer
-  while (start < end) {
-    // If characters at the current positions are not equal, it's not a palindrome
-    if (cleanedStr[start] !== cleanedStr[end]) {
-      return false;
-    }
-
-    // Move the pointers towards the center
-    start++;
-    end--;
-  }
-
-  // If the loop completes without returning false, the string is a palindrome
   return true;
 }
 
+
 module.exports = isPalindrome;
+
+
+
+
