9d69ad053a0dac3eb72bafe76a0ec894f44ffe55
rohithreddy <yrohithreddy12@gmail.com>
2023-12-18 16:21:05 +0530

    countvowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -1,14 +1,5 @@
 function countVowels(str) {
-  const lowerCaseStr = str.toLowerCase();
-  let vowelCount = 0;
-
-  for (let i = 0; i < lowerCaseStr.length; i++) {
-    if (lowerCaseStr[i] === 'a' || lowerCaseStr[i] === 'e' || lowerCaseStr[i] === 'i' || lowerCaseStr[i] === 'o' || lowerCaseStr[i] === 'u') {
-      vowelCount++;
-    }
-  }
-
-  return vowelCount;
+    // Your code here
 }
 
 module.exports = countVowels;
