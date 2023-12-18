ffac4501ee88f94f630a9ad9ac48f7c3320859db
rohithreddy <yrohithreddy12@gmail.com>
2023-12-18 16:21:05 +0530

    countvowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -1,5 +1,23 @@
 function countVowels(str) {
-    // Your code here
+  // Convert the string to lowercase to handle both uppercase and lowercase vowels
+  const lowerCaseStr = str.toLowerCase();
+
+  // Initialize a counter for vowels
+  let vowelCount = 0;
+
+  // Loop through each character in the string
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+      // Check if the character is a vowel
+      if (lowerCaseStr[i] === 'a' || lowerCaseStr[i] === 'e' || lowerCaseStr[i] === 'i' || lowerCaseStr[i] === 'o' || lowerCaseStr[i] === 'u') {
+          // Increment the vowel count if it's a vowel
+          vowelCount++;
+      }
+  }
+
+  // Return the final count of vowels
+  return vowelCount;
 }
 
-module.exports = countVowels;
+// Test the function
+const testString = "Hello, World!";
+console.log(countVowels(testString)); // Output: 3
